2e6eea5709ce54041512ed5a3f58064927d11e7a
Komal <1813421@brunel.ac.uk>
2019-11-07 20:30:11 +0000

    dicebreaker questions task done

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -16,6 +16,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -66,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         EditText et = this.findViewById(R.id.editText);
         TextView st = this.findViewById(R.id.score);
 
+
      try {
          int numberentered = Integer.parseInt(et.getText().toString());
 
@@ -83,10 +87,25 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception ex){
             Log.e("ButtonErrors", ex.toString());
         }
+         
+        }
 
+    public void on_icebreaker_click(View view){
+        TextView q = this.findViewById(R.id.questionsTextView);
 
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("if you could go anywhere in the world where would you go?");
+        stringArrayList.add("if you were stranded on a desert island, what three things would you want to take with you?");
+        stringArrayList.add("if you could eat only one food for the rest of your life, what would it be?");
+        stringArrayList.add("If you won a million dollars, what is the first thing you would buy");
+        stringArrayList.add("if you could spend the day with one fictional character, who would it be?");
+        stringArrayList.add("if you found a magical lantern and a genie gave you three wishes, what would you wish?");
 
-
+        Random k = new Random();
+        int random = k.nextInt((5+1));
+        q.setText(stringArrayList.get(random));
 
     }
-}
+
+    }
+
